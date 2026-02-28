07604d0a476f8cde548846e17d9507be3a53cd2f
Ricky <purujawa06@gmail.com>
2026-02-28 06:04:41 +0000

    J

--- app/api/tools/stabilizer/status/route.js
+++ app/api/tools/stabilizer/status/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import stabilizer from '../../../../lib/stabilizer';
+import stabilizer from '../../../../../lib/stabilizer';
 
 export const dynamic = 'force-dynamic';
 
